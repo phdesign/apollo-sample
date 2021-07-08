4d21dbfa8c7c80d84fcb7509f75a358edda6d0b6
Paul Heasley <paul@phdesign.com.au>
2021-07-08 21:36:14 +1000

    feat: Loading launch list

--- client/src/pages/launches.tsx
+++ client/src/pages/launches.tsx
@@ -1,11 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
 import { RouteComponentProps } from "@reach/router";
-import gql from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
+import LaunchTile from "../components/launch-tile";
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    __typename
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
+
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 interface LauncesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LauncesProps> = () => {
-  return <div>Got here</div>;
+  const { loading, error, data } = useQuery(GET_LAUNCHES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const loadMore = (event) => {};
+
+  const launchList = data.launches.launches.map((launch: any) => (
+    <div key={launch.id}>
+      <LaunchTile launch={launch}></LaunchTile>
+    </div>
+  ));
+
+  return (
+    <div>
+      {launchList}
+      <button onClick={loadMore}>Load more</button>
+    </div>
+  );
 };
 
 export default Launches;
